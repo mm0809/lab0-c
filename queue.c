d52274e335343b89ce2dab990c80228e5f24258d
mm0809 <opmm0809@gmail.com>
2022-02-20 07:00:07 -0800

    Fix q_remove_head/tail return bug
    q_remove_head/tail didn't remove target if buffer
    is NULL in the previous version. Now, no mater
    the buffer is NULL or not, it will remove the
    target.

--- queue.c
+++ queue.c
@@ -120,18 +120,17 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
     if (!head || list_empty(head))
         return NULL;
 
+    element_t *entry = list_first_entry(head, element_t, list);
+    list_del(&entry->list);
+
     if (sp) {
-        element_t *entry = list_first_entry(head, element_t, list);
         size_t len = strlen(entry->value);
         len = (bufsize - 1) > len ? len : (bufsize - 1);
         memcpy(sp, entry->value, len);
         sp[len] = '\0';
-
-        list_del(&entry->list);
-        return entry;
     }
 
-    return NULL;
+    return entry;
 }
 
 /*
@@ -143,18 +142,17 @@ element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
     if (!head || list_empty(head))
         return NULL;
 
+    element_t *entry = list_last_entry(head, element_t, list);
+    list_del(&entry->list);
+
     if (sp) {
-        element_t *entry = list_last_entry(head, element_t, list);
         size_t len = strlen(entry->value);
         len = (bufsize - 1) > len ? len : (bufsize - 1);
         memcpy(sp, entry->value, len);
         sp[len] = '\0';
-
-        list_del(&entry->list);
-        return entry;
     }
 
-    return NULL;
+    return entry;
 }
 
 /*
